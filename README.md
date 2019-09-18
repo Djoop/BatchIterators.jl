eada26481c90c06b6b7513a3630f6e5f59456fab
CHATALIC Antoine <antoine.chatalic@irisa.fr>
2019-09-18 16:13:54 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Summary 
+
+A very small package providing `BatchIterator(X, bsz[, limit])`, which allows iteration on blocks of columns of X, and `choose_batchsize` which helps finding a good batch size while controlling memory usage.
---
+++ src/BatchIterators.jl
@@ -0,0 +1,55 @@
+module BatchIterators
+
+export BatchIterator
+export choose_batchsize
+
+"""
+	BatchIterator(X, bsz[, limit])
+Wrapper allowing to iterate over batches of `bsz` columns of `X`. `X` can be of any type supporting `size` and 2d indexing. When `limit` is provided, iteration is restricted to the columns of `X[:, 1:limit]`.
+"""
+struct BatchIterator{T}
+	X::T
+	length::Int		# Number of batches
+	bsz::Int		# Batch size
+	limit::Int
+	function BatchIterator(X; batchsize=nothing, limit=size(X,2))
+		@assert limit > 0 && limit ≤ size(X,2)
+		bsz = (batchsize == nothing) ? choose_batchsize(size(X,1), limit) : batchsize
+		nb = ceil(Int, limit/bsz)
+		new{typeof(X)}(X, nb, bsz, limit)
+	end
+end
+
+view_compatible(::Any) = false
+view_compatible(::Array) = true
+view_compatible(bi::BatchIterator) = view_compatible(bi.X)
+
+function Base.iterate(it::BatchIterator, st = 0)
+	st = st + 1				# new state
+	d = st - it.length		# > 0 means overflow, == 0 means last batch
+	cbsz = (d == 0) ? mod(it.limit - 1, it.bsz) + 1 : it.bsz		# Size of current batch
+	if d > 0
+		nothing
+	else
+		view_compatible(it) ? ((@view it.X[:, (st-1)*it.bsz+1:(st-1)*it.bsz+cbsz]), st) : (it.X[:, (st-1)*it.bsz+1:(st-1)*it.bsz+cbsz], st)
+	end
+end
+
+"""
+	choose_batchsize(d, n; maxmemGB = 1.0, maxbatchsize = 2^14, sizeoneB = d*sizeof(Float64))
+
+Computes the size (nb. of columns) of a batch, so that each column of the batch can be converted to a vector of size `sizeoneB` (in bytes) with a total memory constrained by `maxmemGB` (gigabytes).
+"""
+function choose_batchsize(d, n; 
+				   maxmemGB = 1.0, 
+				   maxbatchsize = 2^14,
+				   sizeoneB = d*sizeof(Float64),
+				   forcepow2 = true)
+
+	fullsizeGB = n * sizeoneB/1024^3		# Size of the sketches of all samples
+	batchsize = (fullsizeGB > maxmemGB) ? ceil(Int, n/ceil(Int, fullsizeGB/maxmemGB)) : n
+	batchsize = min(batchsize, maxbatchsize)
+	(forcepow2 && batchsize != n) ? prevpow(2, batchsize) : batchsize
+end
+
+end # module
